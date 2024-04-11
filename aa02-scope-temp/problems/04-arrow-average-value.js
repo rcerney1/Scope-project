d0ab6a7154da50f28bd44a3b3c5b16c314d9e07f
rchinson <rhinson2010@gmail.com>
2024-04-11 13:17:48 -0700

    finished problem 04

--- aa02-scope-temp/problems/04-arrow-average-value.js
+++ aa02-scope-temp/problems/04-arrow-average-value.js
@@ -10,7 +10,11 @@ arrowAvgValue([2, 3, 7]); // => 4
 arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
 
-// Your code here 
+const arrowAvgValue = (nums) => (nums.reduce((acc,curr) => acc+curr,0 ))/nums.length;
+
+console.log(arrowAvgValue([10, 20])); // => 15
+console.log(arrowAvgValue([2, 3, 7])); // => 4
+console.log(arrowAvgValue([100, 60, 64])); // => 74.66666666666667
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
